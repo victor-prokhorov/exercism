61860180342214ab6dfb236be0dd724aa54ac4bd
Victor Prokhorov <mail@victorprokhorov.com>
2024-07-13 15:04:28 +0200

    rust minesweeper slightly more optimized

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -3,12 +3,11 @@ extern crate test;
 
 // kudos to the author for this offsets idea
 // https://exercism.org/tracks/rust/exercises/minesweeper/solutions/kstep
-#[rustfmt::skip]
-static OFFSETS: &[(isize, isize)] = &[
-    (-1, -1), (0, -1), (1, -1),
-    (-1,  0),          (1,  0),
-    (-1,  1), (0,  1), (1,  1),
-];
+
+// i want to store and reuse offsets for subsequent rows
+static HI_OFFSETS: &[(isize, isize)] = &[(-1, -1), (-1, 0), (-1, 1)];
+static MI_OFFSETS: &[(isize, isize)] = &[(0, -1), (0, 0), (0, 1)];
+static LO_OFFSETS: &[(isize, isize)] = &[(1, -1), (1, 0), (1, 1)];
 
 fn proc(field: &[&str], cord: (usize, usize), offset: (isize, isize)) -> u8 {
     let Ok::<usize, _>(i) = (cord.0 as isize + offset.0).try_into() else {
@@ -28,20 +27,53 @@ fn proc(field: &[&str], cord: (usize, usize), offset: (isize, isize)) -> u8 {
     }
 }
 
+#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Cell {
+    hi: u8,
+    mi: u8,
+    lo: u8,
+}
+
+impl Cell {
+    fn total(&self) -> u8 {
+        self.hi + self.mi + self.lo
+    }
+}
+
 #[must_use]
 pub fn annotate(field: &[&str]) -> Vec<String> {
     let mut out: Vec<String> = Vec::with_capacity(field.len());
+    let mut prev_cells: Vec<Cell> = Vec::new();
     for (i, r) in field.iter().enumerate() {
         let mut s = String::with_capacity(field.first().unwrap().len());
         let r = r.as_bytes();
         for (j, x) in r.iter().enumerate() {
-            let mut cell_result = 0;
-            for offset in OFFSETS {
-                cell_result += proc(field, (i, j), *offset);
-            }
+            let total = if let Some(cell) = prev_cells.get_mut(j) {
+                cell.hi = cell.mi;
+                cell.mi = cell.lo;
+                cell.lo = 0;
+                for offset in LO_OFFSETS {
+                    cell.lo += proc(field, (i, j), *offset);
+                }
+                cell.total()
+            } else {
+                let mut cell = Cell::default();
+                for offset in HI_OFFSETS {
+                    cell.hi += proc(field, (i, j), *offset);
+                }
+                for offset in MI_OFFSETS {
+                    cell.mi += proc(field, (i, j), *offset);
+                }
+                for offset in LO_OFFSETS {
+                    cell.lo += proc(field, (i, j), *offset);
+                }
+                let total = cell.total();
+                prev_cells.push(cell);
+                total
+            };
             s.push(match x {
-                b' ' if cell_result == 0 => ' ',
-                b' ' => char::from(b'0' + cell_result),
+                b' ' if total == 0 => ' ',
+                b' ' => char::from(b'0' + total),
                 _ => '*',
             });
         }
@@ -116,6 +148,7 @@ mod tests {
     fn bench_sparse(b: &mut Bencher) {
         // iteration    2          7,704.31 ns/iter (+/- 164.31)
         // iteration    3          3,963.78 ns/iter (+/- 133.49)
+        // iteration    4          3,272.93 ns/iter (+/- 72.51)
         b.iter(|| black_box(annotate(black_box(&SPARSE_MINEFIELD))));
     }
 
@@ -123,6 +156,7 @@ mod tests {
     fn bench_dense(b: &mut Bencher) {
         // iteration    2          8,359.85 ns/iter (+/- 131.41)
         // iteration    3          4,666.15 ns/iter (+/- 80.93)
+        // iteration    4          3,514.33 ns/iter (+/- 180.53)
         b.iter(|| black_box(annotate(black_box(&DENSE_MINEFIELD))));
     }
 }
