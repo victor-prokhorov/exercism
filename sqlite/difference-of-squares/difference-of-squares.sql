9727fc8a576488852dbd014494d22790aebebdbe
Victor Prokhorov <mail@victorprokhorov.com>
2024-08-27 21:41:53 +0200

    diff of squares case when then

---
+++ sqlite/difference-of-squares/difference-of-squares.sql
@@ -0,0 +1,10 @@
+-- Schema: CREATE TABLE "difference-of-squares" ("number" INT", property" TEXT, "result" INT);
+-- Task: update the difference-of-squares table and set the result based on the number and property fields.
+-- select distinct property from "difference-of-squares";
+update "difference-of-squares"
+set result = case property
+when "squareOfSum" then pow(number * (number + 1) / 2, 2)
+when "sumOfSquares" then number * (number + 1) * (2 * number + 1) / 6
+when "differenceOfSquares" then pow(number * (number + 1) / 2, 2) - number * (number + 1) * (2 * number + 1) / 6
+end;
+
