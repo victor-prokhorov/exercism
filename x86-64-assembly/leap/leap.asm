d4a2547ec73a90db99fdf6289e385c8cd90fb931
Victor Prokhorov <mail@victorprokhorov.com>
2025-01-11 18:36:33 +0100

    kudos to Silva97 i wouldnt came with this macro usage

--- x86-64-assembly/leap/leap.asm
+++ x86-64-assembly/leap/leap.asm
@@ -1,33 +1,29 @@
+; https://exercism.org/tracks/x86-64-assembly/exercises/leap/solutions/Silva97
+
 section .text
+
+%macro divisible 2
+    mov rax, %1
+    xor rdx, rdx
+    mov rcx, %2
+    div rcx
+    test rdx, rdx
+%endmacro
 
 global leap_year
 leap_year:
-	xor rax, rax		; returned boolean
-	mov rax, rdi		; move year from argument to dividend
-	xor rdx, rdx		; high part of dividend
-	mov rcx, 4		; divisor
-	div rcx			; quotient `rax = rax / rcx`
-				; remainder `rdx = rax % rcx`
-	test rdx, rdx
-	jne not_leap
-				; naturally at this point we already know `rdx` is zero
-	mov rcx, 100		; divisor
-	mov rax, rdi		; `rax` were trashed, `rdi` still holds year	
-	div rcx
-	test rdx, rdx
-	jne leap
-	mov rcx, 400		; here we go again
-	mov rax, rdi
-	div rcx
-	test rdx, rdx
-	jne not_leap
+    divisible rdi, 4
+    jne not_leap
+    divisible rdi, 100
+    jne leap
+    divisible rdi, 400
+    jne not_leap
 leap:
-	mov rax, 1
-	ret 
+    mov rax, 1
+    ret 
 not_leap:
-	xor rax, rax
-	ret
-	
+    xor rax, rax
+    ret
 
 %ifidn __OUTPUT_FORMAT__,elf64
 section .note.GNU-stack noalloc noexec nowrite progbits
