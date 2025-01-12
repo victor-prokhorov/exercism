5407b378868d826c86ae094868a803dad5a4e972
Victor Prokhorov <mail@victorprokhorov.com>
2025-01-12 16:53:12 +0100

    8664 reverse string with pre increment and tmp val

---
+++ x86-64-assembly/reverse-string/reverse_string.asm
@@ -0,0 +1,30 @@
+section .text
+
+; `rdi` pointer to string to reverse, we traverse it left to right, only `dil` non zero
+; `[rsi - 1]` will point to the last character, will be moved right to left
+; `rax` or `al` more precisely will hold value to swap, left hand side
+; `rdx` will hold temporary character of 1 byte, thus only `dl` will be used, right hand side
+global reverse
+reverse:
+        mov     rsi, rdi
+find_last_position:
+        cmp     byte [rsi], 0
+        jz      swap
+        inc     rsi
+        jmp     find_last_position
+swap:
+        dec     rsi
+        cmp     rdi, rsi
+        jge     exit
+        mov     al, byte [rdi]
+        mov     dl, byte [rsi]
+        mov     byte [rdi], dl
+        mov     byte [rsi], al
+        inc     rdi
+        jmp     swap
+exit:
+        ret
+
+%ifidn __OUTPUT_FORMAT__,elf64
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
