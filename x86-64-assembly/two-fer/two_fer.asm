9aafdf32dc3a7a1f44913f2cf41f57650c944c88
Victor Prokhorov <mail@victorprokhorov.com>
2025-01-13 22:08:29 +0100

    two fer

--- x86-64-assembly/two-fer/two_fer.asm
+++ x86-64-assembly/two-fer/two_fer.asm
@@ -28,6 +28,16 @@ write_name:
     je prepare_write_you
     ; i'm about to copy the whole loop block, the only thing that changes is src and dst
     ; feels like a good macro use case
+    xor r10, r10
+    lea r11, [rdi] ; could use rdi at this point instead of r11
+write_loop:
+    mov al, byte [r10 + r11]
+    cmp al, 0
+    je prepare_rhs
+    mov byte [rsi], al
+    inc r10
+    inc rsi
+    jmp write_you
 prepare_write_you:
     xor r10, r10
     lea r11, [you]
@@ -51,6 +61,7 @@ write_rhs:
     inc rsi
     jmp write_rhs
 exit:
+    mov byte [rsi], 0 ; if name is not null we need to add null termination
     ret
 
 %ifidn __OUTPUT_FORMAT__,elf64
