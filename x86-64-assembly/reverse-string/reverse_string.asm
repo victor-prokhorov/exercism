2856bfb8a13bbbb27edb189a82139e9795319b76
Victor Prokhorov <mail@victorprokhorov.com>
2025-01-19 14:37:47 +0100

    mentoring session `default rel` setting

--- x86-64-assembly/reverse-string/reverse_string.asm
+++ x86-64-assembly/reverse-string/reverse_string.asm
@@ -1,29 +1,32 @@
+default rel
+section .bss
+buffer resb 128
 section .text
-
-; `rdi` pointer to string to reverse, we traverse it left to right, only `dil` non zero
-; `[rsi - 1]` will point to the last character, will be moved right to left
-; `rax` or `al` more precisely will hold value to swap, left hand side
-; `rdx` will hold temporary character of 1 byte, thus only `dl` will be used, right hand side
 global reverse
 reverse:
-        mov     rsi, rdi
-find_last_position:
-        cmp     byte [rsi], 0
-        jz      swap
-        inc     rsi
-        jmp     find_last_position
-swap:
-        cmp     rdi, rsi - 1
-        jge     exit
-        mov     al, byte [rdi]
-        mov     dl, byte [rsi - 1]
-        mov     byte [rdi], dl
-        mov     byte [rsi - 1], al
-        inc     rdi
-        dec     rsi
-        jmp     swap
-exit:
-        ret
+	mov rax, rdi
+	lea rsi, [buffer]
+.copy_to_buffer:
+	mov dl, byte [rax]
+	cmp dl, 0
+	jz .setup_reversing
+	mov byte [rsi], dl
+	inc rax
+	inc rsi
+	jmp .copy_to_buffer
+.setup_reversing:
+	dec rsi
+	mov rax, rdi
+.copy_from_buffer:
+	mov dl, byte [rsi]
+	mov byte [rax], dl
+	dec rsi
+	inc rax
+	mov dl, byte [rax]
+	cmp dl, 0
+	jnz .copy_from_buffer
+.exit:
+	ret
 
 %ifidn __OUTPUT_FORMAT__,elf64
 section .note.GNU-stack noalloc noexec nowrite progbits
